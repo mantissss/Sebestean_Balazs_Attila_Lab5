baaa44ff305a3a5c7b10be0f7705ea6009be7726
mantissss <sebesteanattilo@yahoo.com>
2021-11-30 17:26:56 +0200

    Lab5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,16 @@ namespace Sebestean_Balazs_Attila_Lab5
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // customerViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource inventoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // inventoryViewSource.Source = [generic data source]
+        }
     }
 }
